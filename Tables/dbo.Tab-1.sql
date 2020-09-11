7332407b45ccd4c8ebdb2628fa7d947203226750
Ravi <sangana575@gmail.com>
2020-09-11 16:37:57 +0530

    Created a new table Tab-1

---
+++ Tables/dbo.Tab-1.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [dbo].[Tab-1]
+(
+[sno] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[id] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[name] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+) ON [PRIMARY]
+GO
