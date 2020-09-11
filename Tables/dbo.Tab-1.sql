ad8b485483554551014a5f11950739d866c15e59
sanganateja <34955101+sanganateja@users.noreply.github.com>
2020-09-11 16:41:39 +0530

    Update dbo.Tab-1.sql
    Added row emp

--- Tables/dbo.Tab-1.sql
+++ Tables/dbo.Tab-1.sql
@@ -3,5 +3,6 @@ CREATE TABLE [dbo].[Tab-1]
 [sno] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [id] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [name] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[emp] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
