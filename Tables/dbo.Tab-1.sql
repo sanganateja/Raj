14199eca6e8da67d815e75535081746c4d6e5f3a
sanganateja <34955101+sanganateja@users.noreply.github.com>
2020-09-14 16:23:22 +0530

    Added a new column

--- Tables/dbo.Tab-1.sql
+++ Tables/dbo.Tab-1.sql
@@ -4,6 +4,7 @@ CREATE TABLE [dbo].[Tab-1]
 [id] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [name] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [emp] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[salary] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[salary] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[jobtitle] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
